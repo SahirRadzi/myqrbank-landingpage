d98e6a805a4c9cd2c597685001cb6fa74fd13048
SahirRadzi <sahir.radzi@gmail.com>
2024-10-08 15:04:54 +0800

    landingpage

---
+++ main.js
@@ -0,0 +1,15 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".container .letter-qr", {
+  duration: 1000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".container img", {
+  duration: 1000,
+  delay: 1500,
+});
