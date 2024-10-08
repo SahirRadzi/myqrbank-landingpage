702478651d731e3a13ae7a865d8eedb9459cc349
SahirRadzi <sahir.radzi@gmail.com>
2024-10-08 19:58:46 +0800

    updated title & animator

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const scrollRevealOption = {
   duration: 1000,
 };
 
-ScrollReveal().reveal(".container .letter-qr", {
+ScrollReveal().reveal(".container .letter-now", {
   duration: 1000,
   delay: 1000,
 });
@@ -12,4 +12,21 @@ ScrollReveal().reveal(".container .letter-qr", {
 ScrollReveal().reveal(".container img", {
   duration: 1000,
   delay: 1500,
+});
+
+ScrollReveal().reveal(".container .text__left", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".container .text__right", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".container .generate-now", {
+  duration: 2500,
+  delay: 2500,
 });
